89050d95ef2f524c840522dc44c3415787338545
Dania-A <danya.abdaljawad@gmail.com>
2017-12-08 16:54:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ran/parcellable/ShowStudentActivity.java
@@ -0,0 +1,19 @@
+package com.example.ran.parcellable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ShowStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_student);
+
+        //TODO receive the list and show a list view of student
+
+    }
+}
---
+++ app/src/main/java/com/example/ran/parcellable/Student.java
@@ -0,0 +1,31 @@
+package com.example.ran.parcellable;
+
+/**
+ * Created by abdulazizalawshan on 11/6/17.
+ */
+
+public class Student {
+    private String name;
+    private int id;
+
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
